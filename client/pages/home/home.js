f9f37a0ba5f4127b02e90f96f575b3ef7576c745
fujianlian <fujianlian@126.com>
2019-04-18 11:15:00 +0800

    首页静态布局

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -5,62 +5,105 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 商品列表
+    productList: [{
+      id: 1,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+      name: '商品1',
+      price: 100,
+      source: '国内·广东',
+    }, {
+      id: 2,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
+      name: '商品2',
+      price: 200,
+      source: '国内·广东',
+    }, {
+      id: 3,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product3.jpg',
+      name: '商品3',
+      price: 300,
+      source: '国内·广东',
+    }, {
+      id: 4,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product4.jpg',
+      name: '商品4',
+      price: 400,
+      source: '国内·广东',
+    }, {
+      id: 5,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product5.jpg',
+      name: '商品5',
+      price: 500,
+      source: '国内·广东',
+    }, {
+      id: 6,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product5.jpg',
+      name: '商品6',
+      price: 600,
+      source: '国内·广东',
+    }, {
+      id: 7,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product5.jpg',
+      name: '商品7',
+      price: 700,
+      source: '国内·广东',
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
